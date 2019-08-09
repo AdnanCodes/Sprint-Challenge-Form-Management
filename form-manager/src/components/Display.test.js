c7cca7d5d2daa8e2571e19b206eb53e9cc06867b
Adnan <adnan.chowdhury@outlook.com>
2019-08-09 12:19:36 -0500

    Added test for event testing on the button

--- form-manager/src/components/Display.test.js
+++ form-manager/src/components/Display.test.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
 import Display from "./Display";
 
 describe("<Display />", () => {
   it("renders without crashing", () => {
     render(<Display />);
+  });
+  it("button being used", () => {
+    let clicked = false;
+    const { getByText } = render(<Display btn={() => (clicked = true)} />);
+    const button = getByText(/Show me some secret data/i);
+    fireEvent.click(button);
+    expect(clicked).toBe(true);
   });
 });
